8713e3e64af2f13dfc261e29811804c2bfa5c771
V-Markovskis <vlads.markovskis@gmail.com>
2023-11-24 20:35:07 +0200

    search for all input fields

--- src/scripts.ts
+++ src/scripts.ts
@@ -5,6 +5,7 @@ import $ from 'jquery';
 const countryInput = document.querySelector<HTMLInputElement>('.country-name');
 const columnContainer = document.querySelector<HTMLElement>('js-column-container');
 const searchButton = document.querySelector<HTMLButtonElement>('.search-button');
+const formContainer = document.querySelector<HTMLFormElement>('.js-form-container');
 
 //create type for each country
 type Country = {
@@ -26,22 +27,66 @@ type Country = {
     isoCode: string;
 };
 
-const countryElement = document.querySelector('.row-one');
-console.log(countryElement);
 
-const loadCountryDB = () => {
+searchButton.addEventListener('click', () => {
+    const inputs = formContainer.querySelectorAll<HTMLInputElement>('input');
+    const activeInput = Array.from(inputs).find((input) => input.value.trim() !== '');
+
+    let category: keyof Country | undefined;
+
+    const inputName = activeInput.getAttribute('name');
+
+    switch (inputName) {
+        case 'country-name':
+            category = 'name';
+            break;
+        case 'capital-name':
+            category = 'capital';
+            break;
+        case 'currency-name':
+            category = 'currency';
+            break;
+        case 'language-name':
+            category = 'language';
+            break;
+        default:
+            console.log('switch case error');
+            break;
+    }
+
+    if (category) {
+        loadCountryDB(category, activeInput);
+    }
+});
+
+const countryElement = document.querySelector('.row-one');
+
+
+const loadCountryDB = (paramKey: keyof Country, activeInput: HTMLInputElement) => {
     const result = axios.get<Country[]>('http://localhost:3004/countries');
     let db: { countries: Country[] };
 
     result.then(({ data }) => {
         db = { countries: data };
         console.log(db);
-        searchButton.addEventListener('click', () => {
             //get value from input field
-            const searchCountry = countryInput.value.toLowerCase();
+            const searchCountry = activeInput.value.toLowerCase();
+            console.log('searchCountry:', searchCountry);
            
             //filter countries by entered country
-            const filteredCountries = db.countries.filter((country) => country.name.toLowerCase().includes(searchCountry));
+            const filteredCountries = db.countries.filter((country) => {
+                const fieldValue = country[paramKey]; //exp. same for country.name
+            
+                if (typeof fieldValue === 'object' && fieldValue !== null && 'name' in fieldValue) {
+                  return fieldValue.name.toLowerCase().includes(searchCountry);
+                }
+            
+                if (typeof fieldValue === 'string') {
+                  return fieldValue.toLowerCase().includes(searchCountry);
+                }
+                return false;
+
+            });
             console.log('Filtered country', filteredCountries);
     
             //clear field all td elements text
@@ -50,6 +95,8 @@ const loadCountryDB = () => {
             firstRowTds.forEach((td) => {
                 td.textContent = '';
             });
+
+            activeInput.value = '';
             
             if (countryElement) {
                 if (filteredCountries.length > 0) {
@@ -57,15 +104,12 @@ const loadCountryDB = () => {
                     console.log('firstCountry', firstCountry);
                     firstRowTds[0].textContent = firstCountry.name;
                     firstRowTds[1].textContent = firstCountry.capital;
-                    firstRowTds[2].textContent = firstCountry.currency.code;
-                    firstRowTds[3].textContent = firstCountry.language.code;
+                    firstRowTds[2].textContent = firstCountry.currency.name;
+                    firstRowTds[3].textContent = firstCountry.language.name;
                 }
             }
-        });
-    }).catch((error) => {
+        }).catch((error) => {
         console.error('Error loading database:', error);
     });
 };
-
-loadCountryDB();
 
